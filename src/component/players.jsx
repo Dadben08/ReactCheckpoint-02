69fcce04ad05ad15dbd9f64f0aa014ee7a75f366
Dadben08 <Dadben08@gmail.comcleargit config --global user.email Dadben08@gmail.com>
2024-08-10 12:51:14 +0100

    commit

---
+++ src/component/players.jsx
@@ -0,0 +1,38 @@
+// src/players.js
+const players = [
+    {
+      name: "Lionel Messi",
+      team: "Paris Saint-Germain",
+      nationality: "Argentina",
+      jerseyNumber: 30,
+      age: 34,
+      imageUrl: "public/messi.jpg"
+    },
+    {
+      name: "Cristiano Ronaldo",
+      team: "Manchester United",
+      nationality: "Portugal",
+      jerseyNumber: 7,
+      age: 36,
+      imageUrl: "public/Cristiano_ronaldo.jpg"
+    },
+    {
+      name: "Neymar Jr",
+      team: "Paris Saint-Germain",
+      nationality: "Brazil",
+      jerseyNumber: 10,
+      age: 29,
+      imageUrl: "public/Naymer.jpg"
+    },
+    {
+      name: "Kylian Mbappe",
+      team: "Paris Saint-Germain",
+      nationality: "France",
+      jerseyNumber: 7,
+      age: 22,
+      imageUrl: "public/mbappe.jpg"
+    }
+  ];
+  
+  export default players;
+  
